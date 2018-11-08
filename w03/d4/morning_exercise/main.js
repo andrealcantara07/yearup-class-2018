e77a0724933a247f149c35dd8b32daefa584713b
Andre Alcantara-Williams <aalcantarawillia@nyc.yearup.org>
2018-11-08 09:34:34 -0500

    added comments

--- w03/d4/morning_exercise/main.js
+++ w03/d4/morning_exercise/main.js
@@ -7,15 +7,18 @@ const quotes = [
   '"The most important parts,are the ones that are unseen. The wings don’t make you fly, and the crown don’t make you king." - Lupe',
   '"I sell ice in the winter, I sell fire in hell, I am a hustler baby, I’ll sell water to a well" - Jay Z'
 ];
+//TARGET ELEMENT H1 BY SELECTING ID
 let answer = document.getElementById('quote');
-
+//USE RANDOM METHOD AND USE FLOOR METHOD TO EXCLUDE DECIMALS
+//TIMES IT BY THE LENGTH OF THE ARRAY 'QUOTES'
+//USE HTML METHOD TO TARGET HTML AND EQUAL IT TO THE LENGTH OF THE QUOTE
 function randomQuote(){
   let randomSum = Math.floor(Math.random() * quotes.length);
   answer.innerHTML = quotes[randomSum];
 }
-
+//TARGET ELEMENT BUTTON TAG
 let button = document.querySelector('button');
-
+//ADD EVENT CLICK AND RECALL FUNCTION
 button.addEventListener('click', randomQuote);
-
+//CALL FUNCTION
  randomQuote();
